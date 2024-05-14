dafc64ec432e85746f9aaf13beffba6edc43e845
Ankit Bansal <31541954+ankitbansal6@users.noreply.github.com>
2024-05-14 16:13:35 +0530

    Create netflix_raw.sql

---
+++ netflix_raw.sql
@@ -0,0 +1,14 @@
+create TABLE [dbo].[netflix_raw](
+	[show_id] [varchar](10) primary key,
+	[type] [varchar](10) NULL,
+	[title] [nvarchar](200) NULL,
+	[director] [varchar](250) NULL,
+	[cast] [varchar](1000) NULL,
+	[country] [varchar](150) NULL,
+	[date_added] [varchar](20) NULL,
+	[release_year] [int] NULL,
+	[rating] [varchar](10) NULL,
+	[duration] [varchar](10) NULL,
+	[listed_in] [varchar](100) NULL,
+	[description] [varchar](500) NULL
+) 
